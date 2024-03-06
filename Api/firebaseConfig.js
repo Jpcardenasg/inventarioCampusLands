43b0c697e00012f5c853c1f4a79cb695af50cb7d
angelrroa <67547536+angelrroa@users.noreply.github.com>
2024-03-06 06:41:03 -0500

    fix: :bug: Configuración de SDK
    Se realizan cambios en el SDK y forma de inicialización de los elementos de Firebase
    
    @CampusLands

--- Api/firebaseConfig.js
+++ Api/firebaseConfig.js
@@ -1,6 +1,4 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,8 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp( firebaseConfig );
-const analytics = getAnalytics( app );
+const app = firebase.initializeApp( firebaseConfig );
 const db = firebase.firestore();
 
 
