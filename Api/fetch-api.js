52139780237ad1df34fb199943d4e37b04bcd374
JuanPabloCardenasGonzalez <jpcardenasgonzalez@gmail.com>
2024-03-06 06:35:56 -0500

    feat: :sparkles: Funciones de fetch y manejo de la información. Información inventario en formato JSON
    fetc-api.js para recuperar la información por medio de un url o path. Se creó el  archivo invetoryCampusLands.json para guardar la información del invetario
    
    @CampusLands

---
+++ Api/fetch-api.js
@@ -0,0 +1,17 @@
+
+
+export async function fetchData( url ) {
+    try {
+        const response = await fetch( url );
+
+        if ( !response.ok ) {
+            throw new Error( 'Failed to fetch data' );
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch ( error ) {
+        throw new Error( 'Error fetching data', error );
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-// archivo principal
+import { fetchData } from "../Api/fetch-api.js";
+
+
+const INVENTORY_URL = '/data/invetoryCampusLands.json';
+
+const invetoryData = await fetchData( INVENTORY_URL );
+
+
+class Item {
+    constructor ( data ) {
+        this.item = data.item;
+        this.transaccion = data.codigoTransaccion;
+        this.formulario = data.formulario;
+        this.empresa = data.empresa;
+        this.fechaCompra = data.fechaCompra;
+        this.nit = data.nit;
+        this.proveedor = data.proveedor;
+        this.descripcionItem = data.descripcionItem;
+        this.serial = data.serial;
+        this.categoria = data.categoria;
+        this.cantidad = data.cantidad;
+        this.ubicacion = data.ubicacion;
+    }
+}
+
+
+
+class Inventory {
+    constructor ( data ) {
+        this.items = data.map( itemData => new Item( itemData ) );
+    }
+
+    addItem( itemData ) {
+        const newItem = new Item( itemData );
+        this.items.push( newItem );
+    }
+
+    showInventory() {
+        this.items.forEach( item => {
+            console.log( item );
+        } );
+    }
+}
+console.log( "asdsadsa" );
