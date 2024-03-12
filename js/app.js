beab20e7f46b0d4b35f729baf46928bdd2278932
JuanPabloCardenasGonzalez <jpcardenasgonzalez@gmail.com>
2024-03-12 16:57:43 -0500

    chore: :truck: Arreglar path para githubpages
    @CampusLands

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-import '/App/Components/navMenu/menu.js';
-import '/App/Components/formularios/formPersona.js';
-import '/App/Components/formularios/formActivo.js';
-import '/App/Components/formularios/formCategoria.js';
-import '/App/Components/formularios/formEstado.js';
-import '/App/Components/formularios/formMarca.js';
-import '/App/Components/formularios/formTipoActivo.js';
-import '/App/Components/modificarForm/editarForm.js';
-import '/App/Components/modificarForm/editarActivo.js';
-import '/App/Components/modificarForm/editarPersona.js';
-import '/App/Components/modificarForm/buscarForm.js';
-import '/App/Components/modificarForm/buscarActivo.js';
-import '/App/Components/modificarForm/buscarPersona.js';
-import '/App/Components/modificarForm/eliminarForm.js';
-import '/js/classes.js';
-import '/js/events.js';
+import '../App/Components/navMenu/menu.js';
+import '../App/Components/formularios/formPersona.js';
+import '../App/Components/formularios/formActivo.js';
+import '../App/Components/formularios/formCategoria.js';
+import '../App/Components/formularios/formEstado.js';
+import '../App/Components/formularios/formMarca.js';
+import '../App/Components/formularios/formTipoActivo.js';
+import '../App/Components/modificarForm/editarForm.js';
+import '../App/Components/modificarForm/editarActivo.js';
+import '../App/Components/modificarForm/editarPersona.js';
+import '../App/Components/modificarForm/buscarForm.js';
+import '../App/Components/modificarForm/buscarActivo.js';
+import '../App/Components/modificarForm/buscarPersona.js';
+import '../App/Components/modificarForm/eliminarForm.js';
+import './classes.js';
+import './events.js';
 
 
